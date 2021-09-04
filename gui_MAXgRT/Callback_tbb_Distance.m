4af40e138fc362ee3177193d1490ae4e809d3a0d
Zhen JI <zhen_ji@hotmail.com>
2021-09-04 15:21:00 -0500

    Ruler style changed

--- gui_MAXgRT/Callback_tbb_Distance.m
+++ gui_MAXgRT/Callback_tbb_Distance.m
@@ -6,3 +6,19 @@ function Callback_tbb_Distance(src, evnt)
     h = imdistline(data.Panel.View_Cine.subPanel(TagNo).ssPanel(3).Comp.hAxis.Image);
 %     h2 = images.roi.Line(data.Panel.View_Cine.subPanel(TagNo).ssPanel(3).Comp.hAxis.Image, ...
 %         'Position',[50 50; 100 100]); 
+
+c = get(h, 'Children');
+c(1).FontSize = 12;
+c(1).FontWeight = 'bold';
+
+for n = 2:3
+    c(n).Marker = '.';
+    c(n).MarkerSize = 24;
+    c(n).Color = 'w';
+end
+
+for n = 4:5
+%     c(n).Marker = '.';
+%     c(n).MarkerSize = 24;
+    c(n).Color = 'w';
+end
